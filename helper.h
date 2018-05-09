49e8c05b9e03164778c1272efca487a6af9ccd54
c-beck <mr.cjbeck@gmail.com>
2018-05-09 17:57:10 -0400

    thing

--- helper.h
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-
-#ifndef HELPER_H_
-#define HELPER_H_
-int64_t gcf(int64_t a,int64_t b);
-int64_t lcm(int64_t a,int64_t b);
-void mod_pow(int64_t* ans,int64_t base,int64_t exp,int64_t mod);
-int isPrime(int num);
-#endif
