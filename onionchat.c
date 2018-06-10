dcc01be75f1d7d6234d48a100ec8df7aabdb5afd
c-beck <mr.cjbeck@gmail.com>
2018-06-09 22:53:33 -0400

    starting actual program

--- onionchat.c
+++ onionchat.c
@@ -10,9 +10,19 @@ if(argc == 1)
 else if(argc == 2 || argc == 3)
 {
 //server/node
+if(strcmp(argv[1],"-s")==0)
+{
+//start server
+printf("starting server\n");
+}
+else if(strcmp(argv[1],"-h")==0)
+{
+//check ip,start node
+}
 }
 else
 {
+//too many parameters
 printf("Invalid options.\nUsage:\n    %s [-s][-h ip]\n",argv[0]);
 }
 }
