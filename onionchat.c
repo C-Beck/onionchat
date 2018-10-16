e72053a9083194a09d91737d6f1d49ad75d00d47
c-beck <mr.cjbeck@gmail.com>
2018-10-16 15:34:16 -0400

    added indentation to files. got directory creation, file creation, and access checking working.

--- onionchat.c
+++ onionchat.c
@@ -4,36 +4,36 @@
 
 int main(int argc, char *argv[])
 {
-if(argc == 1)
-{
-//client
-printf("starting client\n");
-}
-else if(argc == 2 && strcmp(argv[1],"-s")==0)
-{
-//start server
-char ip[16];
-get_ip(ip);
-printf("starting server\nServer running at %s\n",ip);
-}
-else if(argc ==3 && strcmp(argv[1],"-h")==0)
-{
-//check ip,start node
-if(!check_ip(argv[2]))
-{
-printf("Starting node.\nConnecting to %s\n",argv[2]);
-}
-else
-{
-//bad IP
-printf("Invalid ip\n");
-return 1;
-}
-}
-else
-{
-//too many parameters
-printf("Invalid options.\nUsage:\n    %s [-s|-h ip]\n",argv[0]);
-return 1;
-}
+	if(argc == 1)
+	{
+		//client
+		printf("starting client\n");
+	}
+	else if(argc == 2 && strcmp(argv[1],"-s")==0)
+	{
+		//start server
+		char ip[16];
+		get_ip(ip);
+		printf("starting server\nServer running at %s\n",ip);
+	}
+	else if(argc ==3 && strcmp(argv[1],"-h")==0)
+	{
+		//check ip,start node
+		if(!check_ip(argv[2]))
+		{
+			printf("Starting node.\nConnecting to %s\n",argv[2]);
+		}
+		else
+		{
+			//bad IP
+			printf("Invalid ip\n");
+			return 1;
+		}
+	}
+	else
+	{
+		//too many parameters
+		printf("Invalid options.\nUsage:\n    %s [-s|-h ip]\n",argv[0]);
+		return 1;
+	}
 }
